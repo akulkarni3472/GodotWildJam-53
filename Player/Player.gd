1e468477071ac9e0644af3e39583b8546e90ee75
Kulkarni <Amrut.Kulkarni@xylem.com>
2023-01-18 17:57:07 -0500

    Fixed basic third-person controller

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,24 +1,48 @@
 extends KinematicBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-export var speed = 7.0
+export var speed = 8.0
 export var jump_force = 20.0
-export var gravity = 9.8
+export var gravity = 50
 
-var _velocity = Vector3.ZERO
-var _snap_vector = Vector3.DOWN
+var velocity = Vector3.ZERO
+var snap_vector = Vector3.DOWN
 
-onready var _spring_arm: SpringArm = $SpringArm
-# onready var _model: Spatial = 
+onready var spring_arm= $SpringArm
+onready var model = $MeshInstance
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spring_arm.add_excluded_object(get_rid())
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	apply_gravity(delta)
+	move()
+	jump()
+	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector3.UP, true)
+	
+func _process(delta):
+	spring_arm.translation = translation
+
+func move():
 	var move_dir = Vector3.ZERO
+	move_dir.x = Input.get_action_strength("right") - Input.get_action_raw_strength("left")
+	move_dir.z = Input.get_action_strength("back") - Input.get_action_raw_strength("forward")
+	move_dir = move_dir.rotated(Vector3.UP, spring_arm.rotation.y).normalized()
+	velocity.x = move_dir.x * speed
+	velocity.z = move_dir.z * speed
+
+func apply_gravity(delta):
+	velocity.y -= delta * gravity
+
+func face_direction():
+	if velocity.length() > 0.2:
+		var look_direction = Vector2(velocity.z, velocity.x)
+
+func jump():
+	var landed = is_on_floor() and snap_vector == Vector3.ZERO
+	var jumping = is_on_floor() and Input.is_action_just_pressed("jump")
+	
+	if jumping:
+		snap_vector = Vector3.ZERO
+		velocity.y = jump_force
+	elif landed:
+		snap_vector = Vector3.DOWN
